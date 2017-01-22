56aefcbb1e77f0b9f7a64457a339c7b5aa0451db
Forman <forman@anyks.com>
2017-01-22 21:36:56 +0300

    Чуть-чуть исправляем

---
+++ src/examples/reg.cpp
@@ -0,0 +1,29 @@
+// clang++ -Wall -O3 -pedantic -ggdb -g -std=c++11 -Werror=vla -o ./bin/http ./reg.cpp
+
+#include <iostream>
+#include <string>
+#include <regex>
+
+int main(int argc, char *argv[])
+{
+  std::string url (argv[1]);
+  unsigned counter = 0;
+
+  std::regex url_regex (
+    R"(^(([^:\/?#]+):)?(//([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?)",
+    std::regex::extended
+  );
+  std::smatch url_match_result;
+
+  std::cout << "Checking: " << url << std::endl;
+
+  if (std::regex_match(url, url_match_result, url_regex)) {
+    for (const auto& res : url_match_result) {
+      std::cout << counter++ << ": " << res << std::endl;
+    }
+  } else {
+    std::cerr << "Malformed url." << std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
