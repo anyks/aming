538213b4db4675077d201c345d029ec7db5a85cc
Yuriy Lobarev <forman@anyks.com>
2017-06-27 19:36:40 +0300

    Добавляем тестовый файл для экспериментов

---
+++ src/examples/tests.cpp
@@ -0,0 +1,156 @@
+// $ g++ -std=c++11 ./examples/tests.cpp -o ./bin/tests
+// $ ./bin/tests
+
+#include <regex>
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <math.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+/**
+ * rtrim Функция усечения указанных символов с правой стороны строки
+ * @param  str строка для усечения
+ * @param  t   список символов для усечения
+ * @return     результирующая строка
+ */
+string & rtrim(string &str, const char * t = " \t\n\r\f\v"){
+	str.erase(str.find_last_not_of(t) + 1);
+	return str;
+}
+/**
+ * ltrim Функция усечения указанных символов с левой стороны строки
+ * @param  str строка для усечения
+ * @param  t   список символов для усечения
+ * @return     результирующая строка
+ */
+string & ltrim(string &str, const char * t = " \t\n\r\f\v"){
+	str.erase(0, str.find_first_not_of(t));
+	return str;
+}
+/**
+ * trim Функция усечения указанных символов с правой и левой стороны строки
+ * @param  str строка для усечения
+ * @param  t   список символов для усечения
+ * @return     результирующая строка
+ */
+string & trim(string &str, const char * t = " \t\n\r\f\v"){
+	return ltrim(rtrim(str, t), t);
+}
+/**
+ * split Метод разбива строки на составляющие
+ * @param  str   исходная строка
+ * @param  delim разделитель
+ * @return       массив составляющих строки
+ */
+vector <string> split(const string str, const string delim){
+	// Результат данных
+	vector <string> result;
+	// Создаем новую строку
+	string value = str;
+	// Убираем пробелы в строке
+	value = trim(value);
+	// Если строка передана
+	if(!value.empty()){
+		string data;
+		string::size_type i = 0;
+		string::size_type j = value.find(delim);
+		u_int len = delim.length();
+		// Выполняем разбиение строк
+		while(j != string::npos){
+			data = value.substr(i, j - i);
+			result.push_back(trim(data));
+			i = ++j + (len - 1);
+			j = value.find(delim, j);
+			if(j == string::npos){
+				data = value.substr(i, value.length());
+				result.push_back(trim(data));
+			}
+		}
+		// Если данные не существуют то устанавливаем строку по умолчанию
+		if(result.empty()) result.push_back(value);
+	}
+	// Выводим результат
+	return result;
+}
+
+/*
+map <string, auto> getDomain(const string str){
+	// Создаем список данных
+	map <string, auto> data;
+	map <string, string> result;
+
+	result.insert(pair <string, string> ("www", "rambler"));
+	data.insert(pair <string, auto> ("com", result));
+	//
+	// Название ключа
+	string key = "";
+	// Разбиваем строку на составляющие
+	vector <string> params = (oldData.empty() ? split(str, ".") : oldData), newParams;
+	// Получаем размер массива
+	const size_t len = params.size();
+	// Если количество элементов больше 1
+	if(len > 1){
+		// Выполняем реверс данных
+		reverse(begin(params), end(params));
+		// Переходим по всему массиву данных
+		for(u_int i = 0; i < len; i++){
+			// Если это первый элемент то запоминаем его
+			if(i == 0) key = params[i];
+			// Формируем новый массив
+			else newParams.push_back(params[i]);
+		}
+	// Копируем первый элемент
+	} else key = params[0];
+	// Заполняем структуру данных
+	data.insert(pair <string, T> (key, rules));
+
+
+	// {"com": {"co": {"rambler": "www"}}};
+	//
+
+	return data;
+}
+*/
+
+/*
+template <typename T, typename E>
+auto compose(T a, E b) -> decltype(a+b) // decltype - позволяет определить тип на основе входного параметра
+{
+   return a+b;
+}
+auto c = compose(2, 3.14); // c - double
+*/
+
+// https://habrahabr.ru/post/182920/
+
+template <typename T, typename E>
+map <T, E> compose(T a, E b){
+   map <T, E> data;
+   data.insert(pair <T, E> (a, b));
+   return data;
+}
+
+
+int main(int len, char * buff[]){
+
+	auto a = compose(string("rambler"), string("www"));
+	a.insert(pair <string, string> (string("anyks"), string("ws")));
+
+	auto b = compose("co", a);
+	auto c = compose("com", b);
+
+	int* p2 = nullptr;
+
+	cout << " ++++++++ " << c["com"]["co"]["anyks"] << endl;
+	// auto d = getDomain("www.rambler.com");
+
+	return 0;
+}
--- src/include/headers2/headers.h
+++ src/include/headers2/headers.h
@@ -8,6 +8,7 @@
 #ifndef _HEADERS2_AMING_
 #define _HEADERS2_AMING_
 
+#include <map>
 #include <regex>
 #include <deque>
 #include <string>
@@ -27,10 +28,81 @@ using namespace std;
 
 /**
  * Headers Класс управления заголовками
+ * Параметры в файле заголовков
+ * [action traffic server method path query agent user group headers]
  */
 class Headers {
 	private:
+		/**
+ 		* Rules Структура правил заголовков
+ 		*/
+		struct Rules {
+			string agent;
+			string query;
+			vector <string> headers;
+		};
+		/*
+		* Список правил (server -> ID -> action -> traffic -> method -> path -> {query, agent, headers})
+		*
+		* server может быть
+		* mac
+		* ip
+		* network
+		* domain (домены могут быть с маской вида www.domain.com, *.domain.com, domain.com, *.domain.*, *.com, *.*)
+		*
+		* ID Может быть
+		* group
+		* user
+		* ident
+		*
+		* ident может быть
+		* mac
+		* ip
+		* network
+		*/
+		/*
+		* id_server ->
+		*             | name ->
+		*             |        | id_user ->
+		*             |        |           | name ->
+		*             |        |           |        | action ->
+		*             |        |           |        |          | traffic ->
+		*             |        |           |        |          |           | method ->
+		*             |        |           |        |          |           |          | path ->
+		*             |        |           |        |          |           |          |        | rules
+		*/
+		map <
+			u_int,
+			unordered_map <
+				string,
+				map <
+					u_int,
+					unordered_map <
+						string,
+						map <
+							bool,
+							map <
+								bool,
+								unordered_map <
+									string,
+									unordered_map <
+										string,
+										Rules
+									>
+								>
+							>
+						>
+					>
+				>
+			>
+		> rules;
 	public:
+		/**
+		 * Headers Конструктор
+		 * @param config конфигурационные данные
+		 * @param log    объект лога для вывода информации
+		 */
+		Headers(Config * config = NULL, LogApp * log = NULL);
 };
 
 #endif // _HEADERS2_AMING_
